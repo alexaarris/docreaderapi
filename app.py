5af2ed8afc0f6cae4c0567b76df330ec83f5e2aa
alexaarris <32832652+alexaarris@users.noreply.github.com>
2017-10-18 21:53:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -70,11 +70,6 @@ def makeYqlQuery(req):
 
 
 def makeWebhookResult(data):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("docpart")
-
-    
     speech = "Summary In September 2017, 2 product groups were above the benchmark unit sales of 1,000 units. W (2,328 units), which accounted for 61.78% of the product groups' unit sales, generated the highest unit sales and T (8 units), which accounted for 0.21% of the product groups' unit sales, generated the lowest unit sales."
 
     print("Summary:")
