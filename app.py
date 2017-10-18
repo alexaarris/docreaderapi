efdb74928d9bee9ed4b9218739c84d6f2f1534e6
alexaarris <32832652+alexaarris@users.noreply.github.com>
2017-10-18 20:40:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -63,11 +63,18 @@ def processRequest(req):
 
 def makeYqlQuery(req):
     docpart = req.get("result").get("parameters").get("docpart")
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + docpart + "')"
+    return docpart
+
+def makeYqlQuery(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "') and u='c'"
 
 
 def makeWebhookResult(data):
     speech = "Summary In September 2017, 2 product groups were above the benchmark unit sales of 1,000 units. W (2,328 units), which accounted for 61.78% of the product groups' unit sales, generated the highest unit sales and T (8 units), which accounted for 0.21% of the product groups' unit sales, generated the lowest unit sales." 
+    docpart = req.get("result").get("parameters").get("docpart")
 
     print("Summary:")
     print(speech)
