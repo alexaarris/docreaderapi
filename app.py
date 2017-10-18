9c0a8d3c3650df461b204cfe2e7007a698ab3c82
alexaarris <32832652+alexaarris@users.noreply.github.com>
2017-10-18 16:21:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -62,9 +62,9 @@ def processRequest(req):
 
 
 def makeYqlQuery(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("summary")
+    city = req.get("result").get("parameters").get("summary")
+    #parameters = result.get("parameters")
+    #city = parameters.get("summary")
 
     return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
